523890ef272d814919d94d68e4d18cc0d23865da
Parad0x-spec <clement.boyer83@gmail.com>
2025-12-19 10:35:37 +0100

    feat: implement functional UI and link logic

--- calculator.js
+++ calculator.js
@@ -8,3 +8,16 @@ function subtract(a, b) {
 function multiply(a, b) {
     return a * b;
 }
+
+function calculate() {
+    const n1 = parseFloat(document.getElementById('num1').value);
+    const n2 = parseFloat(document.getElementById('num2').value);
+    const op = document.getElementById('operator').value;
+    let res = 0;
+
+    if (op === 'add') res = add(n1, n2);
+    if (op === 'subtract') res = subtract(n1, n2);
+    if (op === 'multiply') res = multiply(n1, n2);
+
+    document.getElementById('result').innerText = "Résultat : " + res;
+}
